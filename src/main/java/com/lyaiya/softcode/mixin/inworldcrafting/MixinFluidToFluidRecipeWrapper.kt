a832028234bb5b60b0858425854a27ffc5d00498
Lyaiya <hipsnafoha@outlook.com>
2022-10-30 13:06:12 +0800

    Use ModifyArgs

--- src/main/java/com/lyaiya/softcode/mixin/inworldcrafting/MixinFluidToFluidRecipeWrapper.kt
+++ src/main/java/com/lyaiya/softcode/mixin/inworldcrafting/MixinFluidToFluidRecipeWrapper.kt
@@ -3,34 +3,20 @@ package com.lyaiya.softcode.mixin.inworldcrafting
 import net.minecraft.client.resources.*
 import org.spongepowered.asm.mixin.*
 import org.spongepowered.asm.mixin.injection.*
+import org.spongepowered.asm.mixin.injection.invoke.arg.*
 import xt9.inworldcrafting.integrations.jei.*
 
 @Mixin(FluidToFluidRecipeWrapper::class, remap = false)
 abstract class MixinFluidToFluidRecipeWrapper {
-    @ModifyArg(
+    @ModifyArgs(
         method = ["drawInfo"],
-        at = At(value = "INVOKE", target = "drawStringWithShadow(Ljava/lang/String;FFI)I", ordinal = 0),
-        index = 0,
+        at = At(value = "INVOKE", target = "drawStringWithShadow(Ljava/lang/String;FFI)I")
     )
-    private fun modifyArgDrawStringWithShadow0(text: String): String {
-        return I18n.format("${javaClass.recipeWrapperKey}.consumes_item")
-    }
-
-    @ModifyArg(
-        method = ["drawInfo"],
-        at = At(value = "INVOKE", target = "drawStringWithShadow(Ljava/lang/String;FFI)I", ordinal = 1),
-        index = 0,
-    )
-    private fun modifyArgDrawStringWithShadow1(text: String): String {
-        return I18n.format("${javaClass.recipeWrapperKey}.consumes_item.yes")
-    }
-
-    @ModifyArg(
-        method = ["drawInfo"],
-        at = At(value = "INVOKE", target = "drawStringWithShadow(Ljava/lang/String;FFI)I", ordinal = 2),
-        index = 0,
-    )
-    private fun modifyArgDrawStringWithShadow2(text: String): String {
-        return I18n.format("${javaClass.recipeWrapperKey}.consumes_item.no")
+    private fun modifyArgsDrawStringWithShadow(args: Args) {
+        args.apply {
+            set(0, I18n.format("${javaClass.recipeWrapperKey}.consumes_item"))
+            set(1, I18n.format("${javaClass.recipeWrapperKey}.consumes_item.yes"))
+            set(2, I18n.format("${javaClass.recipeWrapperKey}.consumes_item.no"))
+        }
     }
 }
--- src/main/java/com/lyaiya/softcode/mixin/inworldcrafting/MixinFluidToItemRecipeWrapper.kt
+++ src/main/java/com/lyaiya/softcode/mixin/inworldcrafting/MixinFluidToItemRecipeWrapper.kt
@@ -3,34 +3,20 @@ package com.lyaiya.softcode.mixin.inworldcrafting
 import net.minecraft.client.resources.*
 import org.spongepowered.asm.mixin.*
 import org.spongepowered.asm.mixin.injection.*
+import org.spongepowered.asm.mixin.injection.invoke.arg.*
 import xt9.inworldcrafting.integrations.jei.*
 
 @Mixin(FluidToItemRecipeWrapper::class, remap = false)
 abstract class MixinFluidToItemRecipeWrapper {
-    @ModifyArg(
+    @ModifyArgs(
         method = ["drawInfo"],
-        at = At(value = "INVOKE", target = "drawStringWithShadow(Ljava/lang/String;FFI)I", ordinal = 0),
-        index = 0,
+        at = At(value = "INVOKE", target = "drawStringWithShadow(Ljava/lang/String;FFI)I"),
     )
-    private fun modifyArgDrawStringWithShadow0(text: String): String {
-        return I18n.format("${javaClass.recipeWrapperKey}.consumes_fluid")
-    }
-
-    @ModifyArg(
-        method = ["drawInfo"],
-        at = At(value = "INVOKE", target = "drawStringWithShadow(Ljava/lang/String;FFI)I", ordinal = 1),
-        index = 0,
-    )
-    private fun modifyArgDrawStringWithShadow1(text: String): String {
-        return I18n.format("${javaClass.recipeWrapperKey}.consumes_fluid.yes")
-    }
-
-    @ModifyArg(
-        method = ["drawInfo"],
-        at = At(value = "INVOKE", target = "drawStringWithShadow(Ljava/lang/String;FFI)I", ordinal = 2),
-        index = 0,
-    )
-    private fun modifyArgDrawStringWithShadow2(text: String): String {
-        return I18n.format("${javaClass.recipeWrapperKey}.consumes_fluid.no")
+    private fun modifyArgsDrawStringWithShadow(args: Args) {
+        args.apply {
+            set(0, I18n.format("${javaClass.recipeWrapperKey}.consumes_fluid"))
+            set(1, I18n.format("${javaClass.recipeWrapperKey}.consumes_fluid.yes"))
+            set(2, I18n.format("${javaClass.recipeWrapperKey}.consumes_fluid.no"))
+        }
     }
 }
